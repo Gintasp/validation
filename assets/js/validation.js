c6064dbd8f560883c3c7759b5d134f776f31f935
Gintautas Plonis <gplonis@gmail.com>
2018-11-23 00:31:50 +0000

    Generalize validation steps into a function and validate teams.

--- assets/js/validation.js
+++ assets/js/validation.js
@@ -1,21 +1,29 @@
 const axios = require('axios');
 
-let name = document.getElementById('name');
-let validationResult1 = document.getElementById('validation-result1');
-const validateName = function () {
-    validationResult1.innerText = '...';
-    axios.post(validationResult1.dataset.path, {input: name.value})
-        .then(function(response) {
+const validate = function (el) {
+    el.innerText = '...';
+    axios.post(el.dataset.path, {team: team.value, input: name.value})
+        .then(function (response) {
+            console.log(response.data);
             if (response.data.valid) {
-                validationResult1.innerHTML = ":)";
+                el.innerHTML = ":)";
             } else {
-                validationResult1.innerHTML = ":(";
+                el.innerHTML = ":(";
             }
         })
         .catch(function (error) {
-            validationResult1.innerText = 'Error: ' + error;
+            el.innerText = 'Error: ' + error;
         });
 };
 
-name.onkeyup = validateName;
-name.onchange = validateName;
+let name = document.getElementById('name');
+let team = document.getElementById('team');
+let validationName = document.getElementById('validation-result1');
+let validationTeam = document.getElementById('validation-result2');
+
+name.onkeyup = () => validate(validationName);
+name.onchange = () => validate(validationName);
+
+team.onkeyup = () => validate(validationTeam);
+team.onchange = () => validate(validationTeam);
+
